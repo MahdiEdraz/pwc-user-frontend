ed24464ff9e2297b1c62fce585da094b3e95853e
mahdiedraz <you@example.com>
2021-09-04 23:57:39 +0300

    link managed bean with api

--- src/main/java/com/pwc/user/frontend/controller/DepartmentController.java
+++ src/main/java/com/pwc/user/frontend/controller/DepartmentController.java
@@ -9,24 +9,44 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.primefaces.event.RowEditEvent;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.pwc.user.frontend.model.Department;
+import com.pwc.user.frontend.model.User;
 
 @ManagedBean(name = "departmentController")
 @SessionScoped
 public class DepartmentController implements Serializable {
 	private List<Department> departmentsDetails = new ArrayList();
 	private String newDepartment = "";
-
+	private Client client = ClientBuilder.newClient();
+	private GsonBuilder builder = new GsonBuilder(); 
+    private Gson gson = builder.create() ;
+    private Department department = new Department();
 	@PostConstruct
 	public void init() {
+		 Response response = client
+		           .target("http://localhost:8080/pwc-user-api/rest/department/getDepartmentsDetails")
+		           .request(MediaType.APPLICATION_JSON)
+		           .get();
 
+        String json = response.readEntity(String.class);
+        departmentsDetails = gson.fromJson(json, departmentsDetails.getClass());
 	}
 
-	public void onRowEdit(RowEditEvent event) {
-		Department department = (Department) event.getObject();
+	public void onRowEdit(RowEditEvent<Department> event) {
+		Department department = gson.fromJson(String.valueOf(event.getObject()), Department.class);
+        updateDepartment(department);
 		FacesMessage msg = new FacesMessage("User Edited", String.valueOf(department.getId()));
 		FacesContext.getCurrentInstance().addMessage(null, msg);
 	}
@@ -48,16 +68,45 @@ public class DepartmentController implements Serializable {
 	}
 
 	public void createDepartment() {
-		System.out.println(newDepartment);
+		WebTarget target = client.target("http://localhost:8080/pwc-user-api/rest/department/assignUserToDepartment");
+        Form form = new Form();
+        String userJson = gson.toJson(department);
+        form.param("json", userJson);
+        String responseRes = target
+        		.request(MediaType.APPLICATION_JSON)
+        		.post(Entity.entity(form, MediaType.APPLICATION_JSON),String.class);
 	}
 
-	public void updateDepartment(Department updatedDepartment, int index) {
-		Department department = departmentsDetails.get(index);
+	public void updateDepartment(Department updatedDepartment) {
+		
+		WebTarget target = client.target("http://localhost:8080/pwc-user-api/rest/department/updateUserDepartment");
+        Form form = new Form();
+        String userJson = gson.toJson(updatedDepartment);
+        form.param("json", userJson);
+        String responseRes = target
+        		.request(MediaType.APPLICATION_JSON)
+        		.put(Entity.entity(form, MediaType.APPLICATION_JSON),String.class);
 	}
 
 	public void deleteDepartment(int index) {
+		WebTarget target = client.target("http://localhost:8080/pwc-user-api/rest/department/deleteUserOnProject");
+        Form form = new Form();
+        String userJson = gson.toJson(departmentsDetails.get(index));
+        form.param("json", userJson);
+        String responseRes = target
+        		.request(MediaType.APPLICATION_JSON)
+        		.post(Entity.entity(form, MediaType.APPLICATION_JSON),String.class);
 		departmentsDetails.remove(index);
-		System.out.println();
 	}
+
+	public Department getDepartment() {
+		return department;
+	}
+
+	public void setDepartment(Department department) {
+		this.department = department;
+	}
+	
+	
 
 }
