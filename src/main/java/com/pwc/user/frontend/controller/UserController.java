1621c6c4db75b62432b1228ab42a0af86317bffe
mahdiedraz <you@example.com>
2021-09-04 23:58:24 +0300

    Link managedBean with api

--- src/main/java/com/pwc/user/frontend/controller/UserController.java
+++ src/main/java/com/pwc/user/frontend/controller/UserController.java
@@ -5,12 +5,20 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.primefaces.event.RowEditEvent;
 import org.primefaces.event.TabChangeEvent;
 import org.primefaces.event.TabCloseEvent;
 
-import com.pwc.user.frontend.model.Department;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.pwc.user.frontend.model.User;
 
 import java.io.Serializable;
@@ -21,23 +29,22 @@ import java.util.*;
 public class UserController implements Serializable {
 	private List<User> users = new ArrayList();
 	private User newUser = new User();
-
+	private Client client = ClientBuilder.newClient();
+	private GsonBuilder builder = new GsonBuilder(); 
+    private Gson gson = builder.create() ;
 
 	@PostConstruct
-	public void init() {
-		User user = new User();
-		user.setFirstName("aa");
-		user.setId(1);
-		user.setLastName("fr");
-		user.setUserType("user");
-		User user2 = new User();
-		user2.setFirstName("ahmad");
-		user2.setId(2);
-		user2.setLastName("fr");
-		user2.setUserType("user");
+	public void init()  {
+		 Response response = client
+		    		           .target("http://localhost:8080/pwc-user-api/rest/user/getAllUsers")
+		    		           .request(MediaType.APPLICATION_JSON)
+		    		           .get();
+		 
+		String json = response.readEntity(String.class);
+		users = gson.fromJson(json, users.getClass());
 		
-		users.add(user);
-		users.add(user2);	}
+				
+			}
 	
 	public void onTabChange(TabChangeEvent event) {
         FacesMessage msg = new FacesMessage("Tab Changed", "Active Tab: " + event.getTab().getTitle());
@@ -48,8 +55,9 @@ public class UserController implements Serializable {
         FacesMessage msg = new FacesMessage("Tab Closed", "Closed tab: " + event.getTab().getTitle());
         FacesContext.getCurrentInstance().addMessage(null, msg);
     }
-    public void onRowEdit(RowEditEvent event) {
-    	User user = (User) event.getObject();
+    public void onRowEdit(RowEditEvent<User> event)  {
+    	User user = gson.fromJson(String.valueOf(event.getObject()), User.class);
+    	updateUser(user);
         FacesMessage msg = new FacesMessage("User Edited", String.valueOf(user.getId()));
         FacesContext.getCurrentInstance().addMessage(null, msg);
     }
@@ -62,17 +70,36 @@ public class UserController implements Serializable {
 	public void setUsers(List<User> users) {
 		this.users = users;
 	}
-	public void createUser() {
-		System.out.println(newUser);
+	public void createUser()  {
+		
+		WebTarget target = client.target("http://localhost:8080/pwc-user-api/rest/user/createUser");
+        Form form = new Form();
+        String userJson = gson.toJson(newUser);
+        form.param("userJson", userJson);
+        String responseRes = target
+        		.request(MediaType.APPLICATION_JSON)
+        		.post(Entity.entity(form, MediaType.APPLICATION_JSON),String.class);
+        updateMsg(responseRes);
     }
-	public void updateUser(User updatedUser,int index) {
-		User user =users.get(index);
-        user.setFirstName(updatedUser.getFirstName());
+	public void updateUser(User updatedUser)  {
+		WebTarget target = client.target("http://localhost:8080/pwc-user-api/rest/user/updateUser");
+        Form form = new Form();
+        String userJson = gson.toJson(updatedUser);
+        form.param("userJson", userJson);
+        String responseRes = target
+        		.request(MediaType.APPLICATION_JSON)
+        		.put(Entity.entity(form, MediaType.APPLICATION_JSON),String.class);
     }
 	
 	public void deleteUser( int index) {
+		WebTarget target = client.target("http://localhost:8080/pwc-user-api/rest/user/deleteUser");
+        Form form = new Form();
+        String userJson = gson.toJson(users.get(index));
+        form.param("userJson", userJson);
+        String responseRes = target
+        		.request(MediaType.APPLICATION_JSON)
+        		.post(Entity.entity(form, MediaType.APPLICATION_JSON),String.class);
 		users.remove(index);
-		System.out.println();
 	}
 
 	public User getNewUser() {
@@ -83,5 +110,8 @@ public class UserController implements Serializable {
 		this.newUser = newUser;
 	}
     
-
+     public void updateMsg(String messsag) {
+    	  FacesMessage msg = new FacesMessage(messsag);
+          FacesContext.getCurrentInstance().addMessage(null, msg);
+     }
 }
