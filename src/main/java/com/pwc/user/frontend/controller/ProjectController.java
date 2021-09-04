3f0f8ef4b9a67f28eb6f9f997f95ec7b7cbad7a8
mahdiedraz <you@example.com>
2021-09-04 23:58:35 +0300

    Link With api

--- src/main/java/com/pwc/user/frontend/controller/ProjectController.java
+++ src/main/java/com/pwc/user/frontend/controller/ProjectController.java
@@ -9,9 +9,18 @@ import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
 import javax.faces.context.FacesContext;
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
+import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Form;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.primefaces.event.RowEditEvent;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.pwc.user.frontend.model.Department;
 import com.pwc.user.frontend.model.Project;
 
@@ -20,14 +29,25 @@ import com.pwc.user.frontend.model.Project;
 public class ProjectController  implements Serializable{
 	private List<Project> projectsDetails = new ArrayList();
 	private String newProject = "";
+	private Client client = ClientBuilder.newClient();
+	private GsonBuilder builder = new GsonBuilder(); 
+    private Gson gson = builder.create() ;
+    private Project project = new Project();
 
 	@PostConstruct
 	public void init() {
+		 Response response = client
+		           .target("http://localhost:8080/pwc-user-api/rest/project/getProjectsDetails")
+		           .request(MediaType.APPLICATION_JSON)
+		           .get();
 
+      String json = response.readEntity(String.class);
+      projectsDetails = gson.fromJson(json, projectsDetails.getClass());
 	}
 
 	public void onRowEdit(RowEditEvent event) {
-		Project project = (Project) event.getObject();
+		Project project = gson.fromJson(String.valueOf(event.getObject()), Project.class);
+        updateProject(project);
 		FacesMessage msg = new FacesMessage("project Edited", String.valueOf(project.getId()));
 		FacesContext.getCurrentInstance().addMessage(null, msg);
 	}
@@ -48,17 +68,44 @@ public class ProjectController  implements Serializable{
 	public void setNewProject(String newproject) {
 		this.newProject = newproject;
 	}
-
-	public void createProject() {
-		System.out.println(newProject);
+	
+	public Project getProject() {
+		return project;
 	}
 
-	public void updateproject(Project project, int index) {
-		Project department = projectsDetails.get(index);
+	public void setProject(Project project) {
+		this.project = project;
+	}
+
+	public void createProject() {
+		WebTarget target = client.target("http://localhost:8080/pwc-user-api/rest/project/assignUserToProject");
+        Form form = new Form();
+        String userJson = gson.toJson(project);
+        form.param("json", userJson);
+        String responseRes = target
+        		.request(MediaType.APPLICATION_JSON)
+        		.post(Entity.entity(form, MediaType.APPLICATION_JSON),String.class);
+	}
+
+	public void updateProject(Project project) {
+
+		WebTarget target = client.target("http://localhost:8080/pwc-user-api/rest/project/updateUserProject");
+        Form form = new Form();
+        String json = gson.toJson(project);
+        form.param("json", json);
+        String responseRes = target
+        		.request(MediaType.APPLICATION_JSON)
+        		.put(Entity.entity(form, MediaType.APPLICATION_JSON),String.class);
 	}
 
 	public void deleteProject(int index) {
-		projectsDetails.remove(index);
-		System.out.println();
+		WebTarget target = client.target("http://localhost:8080/pwc-user-api/rest/project/deleteUserOnProject");
+        Form form = new Form();
+        String json = gson.toJson(projectsDetails.get(index));
+        form.param("json", json);
+        String responseRes = target
+        		.request(MediaType.APPLICATION_JSON)
+        		.post(Entity.entity(form, MediaType.APPLICATION_JSON),String.class);
+        projectsDetails.remove(index);
 	}
 }
